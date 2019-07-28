30e155ce3244555e8394b11bee6b420deebdee97
Yunus Bağrıyanık <yunusbgrynk@gmail.com>
2019-07-28 18:39:19 +0300

    pagination

--- src/main/java/com/yunus/bagriyanik/restapi/DataModelController.java
+++ src/main/java/com/yunus/bagriyanik/restapi/DataModelController.java
@@ -3,7 +3,9 @@ package com.yunus.bagriyanik.restapi;
 import com.yunus.bagriyanik.dto.DataModelDto;
 import com.yunus.bagriyanik.service.Impl.DataModelServiceImpl;
 import com.yunus.bagriyanik.util.ApiPaths;
+import com.yunus.bagriyanik.util.PaginationMod;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +27,12 @@ public class DataModelController {
   public ResponseEntity<DataModelDto> getById(@PathVariable(value = "id",required = true) Long id){
     DataModelDto dataModelDto = dataModelServiceImpl.getById(id);
     return ResponseEntity.ok(dataModelDto);
+  }
+
+  @GetMapping("/pagination")
+  public ResponseEntity<PaginationMod<DataModelDto>> getAllPageable(Pageable pageable){
+    PaginationMod<DataModelDto> pages = dataModelServiceImpl.getAllPageable(pageable);
+    return ResponseEntity.ok(pages);
   }
 
 }
